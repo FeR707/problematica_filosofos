fdaa44f5036db9ac1b180565aba0d3f5f94c4cc1
Your Name <you@example.com>
2022-10-12 00:34:09 -0500

    filosofos

---
+++ filos/Form1.Designer.cs
@@ -0,0 +1,236 @@
+﻿
+namespace filos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.filo_1 = new System.Windows.Forms.PictureBox();
+            this.filo_4 = new System.Windows.Forms.PictureBox();
+            this.filo_3 = new System.Windows.Forms.PictureBox();
+            this.filo_5 = new System.Windows.Forms.PictureBox();
+            this.filo_2 = new System.Windows.Forms.PictureBox();
+            this.hambre_1 = new System.Windows.Forms.PictureBox();
+            this.hambre_5 = new System.Windows.Forms.PictureBox();
+            this.hambre_4 = new System.Windows.Forms.PictureBox();
+            this.hambre_3 = new System.Windows.Forms.PictureBox();
+            this.hambre_2 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.filo_1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.filo_4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.filo_3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.filo_5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.filo_2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.hambre_1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.hambre_5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.hambre_4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.hambre_3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.hambre_2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(194, 201);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(181, 187);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // filo_1
+            // 
+            this.filo_1.Image = ((System.Drawing.Image)(resources.GetObject("filo_1.Image")));
+            this.filo_1.Location = new System.Drawing.Point(225, 61);
+            this.filo_1.Name = "filo_1";
+            this.filo_1.Size = new System.Drawing.Size(110, 120);
+            this.filo_1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.filo_1.TabIndex = 2;
+            this.filo_1.TabStop = false;
+            // 
+            // filo_4
+            // 
+            this.filo_4.Image = ((System.Drawing.Image)(resources.GetObject("filo_4.Image")));
+            this.filo_4.Location = new System.Drawing.Point(99, 387);
+            this.filo_4.Name = "filo_4";
+            this.filo_4.Size = new System.Drawing.Size(110, 120);
+            this.filo_4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.filo_4.TabIndex = 3;
+            this.filo_4.TabStop = false;
+            // 
+            // filo_3
+            // 
+            this.filo_3.Image = ((System.Drawing.Image)(resources.GetObject("filo_3.Image")));
+            this.filo_3.Location = new System.Drawing.Point(353, 387);
+            this.filo_3.Name = "filo_3";
+            this.filo_3.Size = new System.Drawing.Size(110, 120);
+            this.filo_3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.filo_3.TabIndex = 4;
+            this.filo_3.TabStop = false;
+            // 
+            // filo_5
+            // 
+            this.filo_5.Image = ((System.Drawing.Image)(resources.GetObject("filo_5.Image")));
+            this.filo_5.Location = new System.Drawing.Point(49, 201);
+            this.filo_5.Name = "filo_5";
+            this.filo_5.Size = new System.Drawing.Size(110, 120);
+            this.filo_5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.filo_5.TabIndex = 5;
+            this.filo_5.TabStop = false;
+            // 
+            // filo_2
+            // 
+            this.filo_2.Image = ((System.Drawing.Image)(resources.GetObject("filo_2.Image")));
+            this.filo_2.Location = new System.Drawing.Point(415, 201);
+            this.filo_2.Name = "filo_2";
+            this.filo_2.Size = new System.Drawing.Size(110, 120);
+            this.filo_2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.filo_2.TabIndex = 6;
+            this.filo_2.TabStop = false;
+            // 
+            // hambre_1
+            // 
+            this.hambre_1.Image = ((System.Drawing.Image)(resources.GetObject("hambre_1.Image")));
+            this.hambre_1.Location = new System.Drawing.Point(225, 61);
+            this.hambre_1.Name = "hambre_1";
+            this.hambre_1.Size = new System.Drawing.Size(110, 120);
+            this.hambre_1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.hambre_1.TabIndex = 7;
+            this.hambre_1.TabStop = false;
+            this.hambre_1.Visible = false;
+            // 
+            // hambre_5
+            // 
+            this.hambre_5.Image = ((System.Drawing.Image)(resources.GetObject("hambre_5.Image")));
+            this.hambre_5.Location = new System.Drawing.Point(49, 201);
+            this.hambre_5.Name = "hambre_5";
+            this.hambre_5.Size = new System.Drawing.Size(110, 120);
+            this.hambre_5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.hambre_5.TabIndex = 8;
+            this.hambre_5.TabStop = false;
+            this.hambre_5.Visible = false;
+            // 
+            // hambre_4
+            // 
+            this.hambre_4.Image = ((System.Drawing.Image)(resources.GetObject("hambre_4.Image")));
+            this.hambre_4.Location = new System.Drawing.Point(99, 387);
+            this.hambre_4.Name = "hambre_4";
+            this.hambre_4.Size = new System.Drawing.Size(110, 120);
+            this.hambre_4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.hambre_4.TabIndex = 9;
+            this.hambre_4.TabStop = false;
+            this.hambre_4.Visible = false;
+            // 
+            // hambre_3
+            // 
+            this.hambre_3.Image = ((System.Drawing.Image)(resources.GetObject("hambre_3.Image")));
+            this.hambre_3.Location = new System.Drawing.Point(353, 387);
+            this.hambre_3.Name = "hambre_3";
+            this.hambre_3.Size = new System.Drawing.Size(110, 120);
+            this.hambre_3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.hambre_3.TabIndex = 10;
+            this.hambre_3.TabStop = false;
+            this.hambre_3.Visible = false;
+            // 
+            // hambre_2
+            // 
+            this.hambre_2.Image = ((System.Drawing.Image)(resources.GetObject("hambre_2.Image")));
+            this.hambre_2.Location = new System.Drawing.Point(415, 201);
+            this.hambre_2.Name = "hambre_2";
+            this.hambre_2.Size = new System.Drawing.Size(110, 120);
+            this.hambre_2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.hambre_2.TabIndex = 11;
+            this.hambre_2.TabStop = false;
+            this.hambre_2.Visible = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.label1.Location = new System.Drawing.Point(190, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(198, 19);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "PRESIONE LA ROSQUILLA";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.ClientSize = new System.Drawing.Size(584, 531);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.hambre_2);
+            this.Controls.Add(this.hambre_3);
+            this.Controls.Add(this.hambre_4);
+            this.Controls.Add(this.hambre_5);
+            this.Controls.Add(this.hambre_1);
+            this.Controls.Add(this.filo_2);
+            this.Controls.Add(this.filo_5);
+            this.Controls.Add(this.filo_3);
+            this.Controls.Add(this.filo_4);
+            this.Controls.Add(this.filo_1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.filo_1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.filo_4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.filo_3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.filo_5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.filo_2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.hambre_1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.hambre_5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.hambre_4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.hambre_3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.hambre_2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox filo_1;
+        private System.Windows.Forms.PictureBox filo_4;
+        private System.Windows.Forms.PictureBox filo_3;
+        private System.Windows.Forms.PictureBox filo_5;
+        private System.Windows.Forms.PictureBox filo_2;
+        private System.Windows.Forms.PictureBox hambre_1;
+        private System.Windows.Forms.PictureBox hambre_5;
+        private System.Windows.Forms.PictureBox hambre_4;
+        private System.Windows.Forms.PictureBox hambre_3;
+        private System.Windows.Forms.PictureBox hambre_2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ filos/Form1.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace filos
+{
+    public partial class Form1 : Form
+    {
+		public bool palillo1 = true, palillo2 = true, palillo3 = true, palillo4 = true, palillo5 = true;//creamos los palillos
+		public Semaphore semaforo = new Semaphore(2,3);//declaramos el semaforo
+		public Form1()
+        {
+            InitializeComponent();
+        }
+		private void pictureBox1_Click(object sender, EventArgs e)
+		{
+            /*for (int i = 0; i < 5; i++)
+			{
+				Thread filo = new Thread(new ThreadStart(iteracion));
+				filo.Name = string.Format("{0}", i + 1);
+				filo.Start();
+			}*/
+            for (int i = 0; i < 5; i++)
+            {
+				Thread filo1 = new Thread(filosofo1);
+				Thread filo2 = new Thread(filosofo2);
+				Thread filo3 = new Thread(filosofo3);
+				Thread filo4 = new Thread(filosofo4);
+				Thread filo5 = new Thread(filosofo5);
+				filo1.Start();
+				filo2.Start();
+				filo3.Start();
+				filo4.Start();
+				filo5.Start();
+			}
+		}
+		private void filosofo()
+		{
+			semaforo.WaitOne();
+			Console.WriteLine("Filosofo {0} esta pensado", Thread.CurrentThread.Name);
+			palillo1 = false;
+			palillo2 = false;
+			Console.WriteLine("Filosofo {0} esta comiendo", Thread.CurrentThread.Name);
+			Thread.Sleep(5000);
+			palillo1 = true;
+			palillo2 = true;
+			Console.WriteLine("Filosofo {0} a terminado de comer", Thread.CurrentThread.Name);
+			semaforo.Release();
+		}
+		private void iteracion()
+		{
+			for (int i = 0; i < 5; i++)
+			{
+				filosofo();
+			}
+		}
+		public void filosofo1()
+		{
+			semaforo.WaitOne();// llamamos a l semaforo
+			if (palillo1 == true && palillo2 == true)//condicion para los palillos
+			{
+				palillo1 = false;//ponemos a los palillos en ocupado "false"
+				palillo2 = false;
+				Invoke((Delegate)new Action(() => {
+					filo_1.Visible = false;//cambiamos las imagenes
+					hambre_1.Visible = true;
+				}));
+				Thread.Sleep(3000);//tiempo de espera 3 segundos
+			}
+			palillo1 = true;//despues de la espera
+			palillo2 = true;//cambiamos los palillos a desocupado "true"
+			Invoke((Delegate)new Action(() => {
+				filo_1.Visible = true;
+				hambre_1.Visible = false;
+			}));
+			semaforo.Release();// finalizacion del semaforo
+		}
+        public void filosofo2()
+		{
+			semaforo.WaitOne();
+			if (palillo2 == true && palillo3 == true)
+			{
+				palillo2 = false;
+				palillo3 = false;
+				Invoke((Delegate)new Action(() => {
+					filo_2.Visible = false;
+					hambre_2.Visible = true;
+				}));
+				Thread.Sleep(3000);
+			}
+			palillo2 = true;
+			palillo3 = true;
+			Invoke((Delegate)new Action(() => {
+				filo_2.Visible = true;
+				hambre_2.Visible = false;
+			}));
+			semaforo.Release();
+		}
+		public void filosofo3()
+		{
+			semaforo.WaitOne();
+			if (palillo3 == true && palillo4 == true)
+			{
+				palillo3 = false;
+				palillo4 = false;
+				Invoke((Delegate)new Action(() => {
+					filo_3.Visible = false;
+					hambre_3.Visible = true;
+				}));
+				Thread.Sleep(3000);
+			}
+			palillo3 = true;
+			palillo4 = true;
+			Invoke((Delegate)new Action(() => {
+				filo_3.Visible = true;
+				hambre_3.Visible = false;
+			}));
+			semaforo.Release();
+		}
+		public void filosofo4()
+		{
+			semaforo.WaitOne();
+			if (palillo4 == true && palillo5 == true)
+			{
+				palillo4 = false;
+				palillo5 = false;
+				Invoke((Delegate)new Action(() => {
+					filo_4.Visible = false;
+					hambre_4.Visible = true;
+				}));
+				Thread.Sleep(3000);
+			}
+			palillo4 = true;
+			palillo5 = true;
+			Invoke((Delegate)new Action(() => {
+				filo_4.Visible = true;
+				hambre_4.Visible = false;
+			}));
+			semaforo.Release();
+		}
+		public void filosofo5()
+		{
+			semaforo.WaitOne();
+			if (palillo5 == true && palillo1 == true)
+			{
+				palillo5 = false;
+				palillo1 = false;
+				Invoke((Delegate)new Action(() => {
+					filo_5.Visible = false;
+					hambre_5.Visible = true;
+				}));
+				Thread.Sleep(3000);
+			}
+			palillo5 = true;
+			palillo1 = true;
+			Invoke((Delegate)new Action(() => {
+				filo_5.Visible = true;
+				hambre_5.Visible = false;
+			}));
+			semaforo.Release();
+		}
+    }
+}
